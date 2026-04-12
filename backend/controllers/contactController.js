23cb4f25a4d077442d5e99c4202250ca5ca762c1
Dipanshu <dipanshudk46107@gmail.com>
2026-04-12 18:08:57 +0530

    migrate from nodemailer to resend

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -1,67 +1,52 @@
+const { Resend } = require("resend")
 
-const nodemailer = require("nodemailer");
+const resend = new Resend(process.env.RESEND_API_KEY)
 
 const contactController = async (req, res) => {
     try {
-        const { name, email, message } = req.body;
+        const { name, email, message } = req.body
 
         // validation
         if (!name || !email || !message) {
             return res.status(400).json({
                 success: false,
                 message: "All fields are required"
-            });
+            })
         }
 
         if (!email.includes("@")) {
             return res.status(400).json({
                 success: false,
                 message: "Invalid email"
-            });
+            })
         }
 
-        // transporter
-        const transporter = nodemailer.createTransport({
-            service: "gmail",
-            port: 587,
-            secure: false, 
-            auth: {
-                user: process.env.EMAIL,
-                pass: process.env.APP_PASSWORD
-            }
-        });
-
-        // mail options
-        const mailOptions = {
-            from: process.env.EMAIL,
-            to: process.env.EMAIL, // you receive message
+        // send email using Resend
+        await resend.emails.send({
+            from: "onboarding@resend.dev",
+            to: process.env.EMAIL, 
             subject: `New message from ${name}`,
-            text: `
-        Name: ${name}
-        Email: ${email}
-        Message: ${message}
+            html: `
+                <h3>New Contact Message</h3>
+                <p><b>Name:</b> ${name}</p>
+                <p><b>Email:</b> ${email}</p>
+                <p><b>Message:</b> ${message}</p>
             `
-        };
+        })
 
-        // send mail
-        await transporter.sendMail(mailOptions);
-
-        // success response
-        res.json({
+        res.status(200).json({
             success: true,
             message: "Email sent successfully"
-        });
+        })
 
     } catch (error) {
-        console.log(error);
+        console.log("ERROR:", error)
 
         res.status(500).json({
             success: false,
-            message: "Error sending email"
-        });
+            message: error.message
+        })
     }
-};
+}
 
-
-
-module.exports = {contactController}
+module.exports = { contactController }
